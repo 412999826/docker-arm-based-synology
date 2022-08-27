a7cc599c6e596a4864c2bbfad56968778d48bb9b
412999826 <68527612+412999826@users.noreply.github.com>
2022-08-27 18:55:53 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+ARCH=aarch64
+DOCKER_VERSION=20.10.9
+DOCKER_DIR=/volume1/docker
+
+echo "下载 docker $DOCKER_VERSION-$ARCH"
+curl "https://download.docker.com/linux/static/stable/$ARCH/docker-$DOCKER_VERSION.tgz" | tar -xz -C /usr/bin --strip-components=1
+
+echo "创建工作目录 $DOCKER_DIR"
+mkdir -p "$DOCKER_DIR"
+
+echo "创建 docker.json 配置文件"
+mkdir -p /etc/docker
+cat <<EOT > "/etc/docker/daemon.json
+{
+  "storage-driver": "vfs",
+  "iptables": false,
+  "data-root": "$DOCKER_DIR"
+}
+EOT
+
+echo "完成。 请添加计划任务并重启NAS."
